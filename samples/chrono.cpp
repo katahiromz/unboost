c76d02924b087a39f429a2cbbcca6a3dbbd342f8
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2016-10-23 09:02:29 +0900

    update chrono sample

--- samples/chrono.cpp
+++ samples/chrono.cpp
@@ -5,16 +5,9 @@
 #include <unboost.hpp>
 
 int main(void) {
+    using namespace unboost::chrono;
     std::cout << "chrono" << std::endl;
     {
-        // 4807 seconds
-        using namespace unboost::chrono;
-        unboost_auto_duration s = hours(1) + 2 * minutes(10) + seconds(70) / 10;
-        std::cout << "1 hour + 2*10 min + 70/10 sec = " << s.count() << " seconds\n";
-        assert(s.count() == 4807);
-    }
-    {
-        using namespace unboost::chrono;
         seconds sec(1);
         std::cout << duration_cast<microseconds>(sec).count() << " microseconds" << std::endl;
         std::cout << duration_cast<milliseconds>(sec).count() << " milliseconds" << std::endl;
@@ -22,6 +15,61 @@ int main(void) {
         assert(duration_cast<microseconds>(sec).count() == 1000000);
         assert(duration_cast<milliseconds>(sec).count() == 1000);
         assert(duration_cast<seconds>(sec).count() == 1);
+    }
+    {
+        milliseconds ms(3);
+        microseconds us = 2 * ms;
+        duration<double, unboost::ratio<1, 30> > hz30(3.5);
+        assert(ms.count() == 3);
+        assert(us.count() == 6000);
+        assert(hz30.count() == 3.5);
+    }
+    {
+        seconds s1(10);
+        seconds s2 = -s1;
+        assert(s2.count() == -10);
+    }
+    {
+        hours h(1);
+        minutes m = ++h;
+        m--;
+        assert(m.count() == 119);
+    }
+    {
+        minutes m(11);
+        m *= 2;
+        m += hours(10);
+        assert(m.count() == 622);
+        assert(duration_cast<hours>(m).count() == 10);
+        m %= hours(1);
+        assert(m.count() == 22);
+    }
+    {
+        unboost_auto_duration s = hours(1) + 2 * minutes(10) + seconds(70) / 10;
+        std::cout << "1 hour + 2*10 min + 70/10 sec = " << s.count() << " seconds\n";
+        assert(s.count() == 4807);
+
+        assert(duration_cast<hours>(s).count() == 1);
+        assert(duration_cast<minutes>(s % hours(1)).count() == 20);
+        assert(duration_cast<seconds>(s % minutes(1)).count() == 7);
+    }
+    {
+        assert(seconds(2) == milliseconds(2000));
+        assert(seconds(61) > minutes(1));
+    }
+    {
+        typedef duration<int, std::ratio<1, 100000000> >        shakes;
+        typedef duration<int, unboost::centi>                   jiffies;
+        typedef duration<float, unboost::ratio<12096,10000> >   microfortnights;
+        typedef duration<float, unboost::ratio<3155,1000> >     nanocenturies;
+        seconds sec(1);
+
+        assert(duration_cast<shakes>(sec).count() == 100000000);
+        assert(duration_cast<jiffies>(sec).count() == 100);
+        double f1 = duration_cast<microfortnights>(sec).count();
+        assert(0.8267 <= f1 && f1 <= 0.8268);
+        double f2 = duration_cast<nanocenturies>(sec).count():
+        assert(0.31695 <= f2 && f2 <= 0.31696);
     }
     {
         //unboost::chrono::time_point<unboost::chrono::steady_clock> t1, t2;
