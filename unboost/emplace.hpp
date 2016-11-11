ca8b0607bd373fcf24c247cce5d9f9263fa8805e
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2016-11-11 12:35:37 +0900

    fix emplace

--- unboost/emplace.hpp
+++ unboost/emplace.hpp
@@ -5,6 +5,7 @@
 #define UNBOOST_EMPLACE_HPP_
 
 #include "unboost.hpp"
+#include "rv_ref.hpp"   // for unboost::forward
 
 // unboost::emplace(obj, ...)
 // unboost::emplace_at(obj, pos, ...)
@@ -23,29 +24,29 @@
                 obj.emplace();
             }
             template <class T, typename ARG1>
-            inline void emplace(T& obj, const ARG1& arg1)
+            inline void emplace(T& obj, ARG1 arg1)
             {
-                obj.emplace(arg1);
+                obj.emplace(forward<ARG1>(arg1));
             }
             template <class T, typename ARG1, typename ARG2>
-            inline void emplace(T& obj, const ARG1& arg1, const ARG2& arg2)
+            inline void emplace(T& obj, ARG1 arg1, ARG2 arg2)
             {
-                obj.emplace(arg1, arg2);
+                obj.emplace(forward<ARG1>(arg1), forward<ARG2>(arg2));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3>
-            inline void emplace(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3)
+            inline void emplace(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3)
             {
-                obj.emplace(arg1, arg2, arg3);
+                obj.emplace(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4>
-            inline void emplace(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4)
+            inline void emplace(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4)
             {
-                obj.emplace(arg1, arg2, arg3, arg4);
+                obj.emplace(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5>
-            inline void emplace(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4, const ARG5& arg5)
+            inline void emplace(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4, ARG5 arg5)
             {
-                obj.emplace(arg1, arg2, arg3, arg4, arg5);
+                obj.emplace(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4), forward<ARG5>(arg5));
             }
 
             //
@@ -57,27 +58,27 @@
                 obj.emplace(pos);
             }
             template <class T, typename POS, typename ARG1>
-            inline void emplace_at(T& obj, const POS& pos, const ARG1& arg1)
+            inline void emplace_at(T& obj, const POS& pos, ARG1 arg1)
             {
                 obj.emplace(pos, arg1);
             }
             template <class T, typename POS, typename ARG1, typename ARG2>
-            inline void emplace_at(T& obj, const POS& pos, const ARG1& arg1, const ARG2& arg2)
+            inline void emplace_at(T& obj, const POS& pos, ARG1 arg1, ARG2 arg2)
             {
                 obj.emplace(pos, arg1, arg2);
             }
             template <class T, typename POS, typename ARG1, typename ARG2, typename ARG3>
-            inline void emplace_at(T& obj, const POS& pos, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3)
+            inline void emplace_at(T& obj, const POS& pos, ARG1 arg1, ARG2 arg2, ARG3 arg3)
             {
                 obj.emplace(pos, arg1, arg2, arg3);
             }
             template <class T, typename POS, typename ARG1, typename ARG2, typename ARG3, typename ARG4>
-            inline void emplace_at(T& obj, const POS& pos, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4)
+            inline void emplace_at(T& obj, const POS& pos, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4)
             {
                 obj.emplace(pos, arg1, arg2, arg3, arg4);
             }
             template <class T, typename POS, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5>
-            inline void emplace_at(T& obj, const POS& pos, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4, const ARG5& arg5)
+            inline void emplace_at(T& obj, const POS& pos, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4, ARG5 arg5)
             {
                 obj.emplace(pos, arg1, arg2, arg3, arg4, arg5);
             }
@@ -91,29 +92,29 @@
                 obj.emplace_front();
             }
             template <class T, typename ARG1>
-            inline void emplace_front(T& obj, const ARG1& arg1)
+            inline void emplace_front(T& obj, ARG1 arg1)
             {
-                obj.emplace_front(arg1);
+                obj.emplace_front(forward<ARG1>(arg1));
             }
             template <class T, typename ARG1, typename ARG2>
-            inline void emplace_front(T& obj, const ARG1& arg1, const ARG2& arg2)
+            inline void emplace_front(T& obj, ARG1 arg1, ARG2 arg2)
             {
-                obj.emplace_front(arg1, arg2);
+                obj.emplace_front(forward<ARG1>(arg1), forward<ARG2>(arg2));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3>
-            inline void emplace_front(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3)
+            inline void emplace_front(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3)
             {
-                obj.emplace_front(arg1, arg2, arg3);
+                obj.emplace_front(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4>
-            inline void emplace_front(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4)
+            inline void emplace_front(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4)
             {
-                obj.emplace_front(arg1, arg2, arg3, arg4);
+                obj.emplace_front(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5>
-            inline void emplace_front(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4, const ARG5& arg5)
+            inline void emplace_front(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4, ARG5 arg5)
             {
-                obj.emplace_front(arg1, arg2, arg3, arg4, arg5);
+                obj.emplace_front(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4), forward<ARG5>(arg5));
             }
 
             //
@@ -125,29 +126,29 @@
                 obj.emplace_back();
             }
             template <class T, typename ARG1>
-            inline void emplace_back(T& obj, const ARG1& arg1)
+            inline void emplace_back(T& obj, ARG1 arg1)
             {
-                obj.emplace_back(arg1);
+                obj.emplace_back(forward<ARG1>(arg1));
             }
             template <class T, typename ARG1, typename ARG2>
-            inline void emplace_back(T& obj, const ARG1& arg1, const ARG2& arg2)
+            inline void emplace_back(T& obj, ARG1 arg1, ARG2 arg2)
             {
-                obj.emplace_back(arg1, arg2);
+                obj.emplace_back(forward<ARG1>(arg1), forward<ARG2>(arg2));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3>
-            inline void emplace_back(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3)
+            inline void emplace_back(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3)
             {
-                obj.emplace_back(arg1, arg2, arg3);
+                obj.emplace_back(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4>
-            inline void emplace_back(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4)
+            inline void emplace_back(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4)
             {
-                obj.emplace_back(arg1, arg2, arg3, arg4);
+                obj.emplace_back(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5>
-            inline void emplace_back(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4, const ARG5& arg5)
+            inline void emplace_back(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4, ARG5 arg5)
             {
-                obj.emplace_back(arg1, arg2, arg3, arg4, arg5);
+                obj.emplace_back(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4), forward<ARG5>(arg5));
             }
         #else   // Not C++11
             //
@@ -160,34 +161,34 @@
                 obj.insert(value_type());
             }
             template <class T, typename ARG1>
-            inline void emplace(T& obj, const ARG1& arg1)
+            inline void emplace(T& obj, ARG1 arg1)
             {
                 typedef typename T::value_type value_type;
-                obj.insert(value_type(arg1));
+                obj.insert(value_type(forward<ARG1>(arg1)));
             }
             template <class T, typename ARG1, typename ARG2>
-            inline void emplace(T& obj, const ARG1& arg1, const ARG2& arg2)
+            inline void emplace(T& obj, ARG1 arg1, ARG2 arg2)
             {
                 typedef typename T::value_type value_type;
-                obj.insert(value_type(arg1, arg2));
+                obj.insert(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2)));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3>
-            inline void emplace(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3)
+            inline void emplace(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3)
             {
                 typedef typename T::value_type value_type;
-                obj.insert(value_type(arg1, arg2, arg3));
+                obj.insert(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3)));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4>
-            inline void emplace(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4)
+            inline void emplace(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4)
             {
                 typedef typename T::value_type value_type;
-                obj.insert(value_type(arg1, arg2, arg3, arg4));
+                obj.insert(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4)));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5>
-            inline void emplace(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4, const ARG5& arg5)
+            inline void emplace(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4, ARG5 arg5)
             {
                 typedef typename T::value_type value_type;
-                obj.insert(value_type(arg1, arg2, arg3, arg4, arg5));
+                obj.insert(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4), forward<ARG5>(arg5)));
             }
 
             //
@@ -200,34 +201,34 @@
                 obj.insert(pos, value_type());
             }
             template <class T, typename POS, typename ARG1>
-            inline void emplace_at(T& obj, const POS& pos, const ARG1& arg1)
+            inline void emplace_at(T& obj, const POS& pos, ARG1 arg1)
             {
                 typedef typename T::value_type value_type;
-                obj.insert(pos, value_type(arg1));
+                obj.insert(pos, value_type(forward<ARG1>(arg1)));
             }
             template <class T, typename POS, typename ARG1, typename ARG2>
-            inline void emplace_at(T& obj, const POS& pos, const ARG1& arg1, const ARG2& arg2)
+            inline void emplace_at(T& obj, const POS& pos, ARG1 arg1, ARG2 arg2)
             {
                 typedef typename T::value_type value_type;
-                obj.insert(pos, value_type(arg1, arg2));
+                obj.insert(pos, value_type(forward<ARG1>(arg1), forward<ARG2>(arg2)));
             }
             template <class T, typename POS, typename ARG1, typename ARG2, typename ARG3>
-            inline void emplace_at(T& obj, const POS& pos, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3)
+            inline void emplace_at(T& obj, const POS& pos, ARG1 arg1, ARG2 arg2, ARG3 arg3)
             {
                 typedef typename T::value_type value_type;
-                obj.insert(pos, value_type(arg1, arg2, arg3));
+                obj.insert(pos, value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3)));
             }
             template <class T, typename POS, typename ARG1, typename ARG2, typename ARG3, typename ARG4>
-            inline void emplace_at(T& obj, const POS& pos, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4)
+            inline void emplace_at(T& obj, const POS& pos, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4)
             {
                 typedef typename T::value_type value_type;
-                obj.insert(pos, value_type(arg1, arg2, arg3, arg4));
+                obj.insert(pos, value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4)));
             }
             template <class T, typename POS, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5>
-            inline void emplace_at(T& obj, const POS& pos, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4, const ARG5& arg5)
+            inline void emplace_at(T& obj, const POS& pos, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4, ARG5 arg5)
             {
                 typedef typename T::value_type value_type;
-                obj.insert(pos, value_type(arg1, arg2, arg3, arg4, arg5));
+                obj.insert(pos, value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4), forward<ARG5>(arg5)));
             }
 
             //
@@ -240,34 +241,34 @@
                 obj.push_front(value_type());
             }
             template <class T, typename ARG1>
-            inline void emplace_front(T& obj, const ARG1& arg1)
+            inline void emplace_front(T& obj, ARG1 arg1)
             {
                 typedef typename T::value_type value_type;
-                obj.push_front(value_type(arg1));
+                obj.push_front(value_type(forward<ARG1>(arg1)));
             }
             template <class T, typename ARG1, typename ARG2>
-            inline void emplace_front(T& obj, const ARG1& arg1, const ARG2& arg2)
+            inline void emplace_front(T& obj, ARG1 arg1, ARG2 arg2)
             {
                 typedef typename T::value_type value_type;
-                obj.push_front(value_type(arg1, arg2));
+                obj.push_front(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2)));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3>
-            inline void emplace_front(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3)
+            inline void emplace_front(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3)
             {
                 typedef typename T::value_type value_type;
-                obj.push_front(value_type(arg1, arg2, arg3));
+                obj.push_front(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3)));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4>
-            inline void emplace_front(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4)
+            inline void emplace_front(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4)
             {
                 typedef typename T::value_type value_type;
-                obj.push_front(value_type(arg1, arg2, arg3, arg4));
+                obj.push_front(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4)));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5>
-            inline void emplace_front(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4, const ARG5& arg5)
+            inline void emplace_front(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4, ARG5 arg5)
             {
                 typedef typename T::value_type value_type;
-                obj.push_front(value_type(arg1, arg2, arg3, arg4, arg5));
+                obj.push_front(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4), forward<ARG5>(arg5)));
             }
 
             //
@@ -280,34 +281,34 @@
                 obj.push_back(value_type());
             }
             template <class T, typename ARG1>
-            inline void emplace_back(T& obj, const ARG1& arg1)
+            inline void emplace_back(T& obj, ARG1 arg1)
             {
                 typedef typename T::value_type value_type;
-                obj.push_back(value_type(arg1));
+                obj.push_back(value_type(forward<ARG1>(arg1)));
             }
             template <class T, typename ARG1, typename ARG2>
-            inline void emplace_back(T& obj, const ARG1& arg1, const ARG2& arg2)
+            inline void emplace_back(T& obj, ARG1 arg1, ARG2 arg2)
             {
                 typedef typename T::value_type value_type;
-                obj.push_back(value_type(arg1, arg2));
+                obj.push_back(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2)));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3>
-            inline void emplace_back(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3)
+            inline void emplace_back(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3)
             {
                 typedef typename T::value_type value_type;
-                obj.push_back(value_type(arg1, arg2, arg3));
+                obj.push_back(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3)));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4>
-            inline void emplace_back(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4)
+            inline void emplace_back(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4)
             {
                 typedef typename T::value_type value_type;
-                obj.push_back(value_type(arg1, arg2, arg3, arg4));
+                obj.push_back(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4)));
             }
             template <class T, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5>
-            inline void emplace_back(T& obj, const ARG1& arg1, const ARG2& arg2, const ARG3& arg3, const ARG4& arg4, const ARG5& arg5)
+            inline void emplace_back(T& obj, ARG1 arg1, ARG2 arg2, ARG3 arg3, ARG4 arg4, ARG5 arg5)
             {
                 typedef typename T::value_type value_type;
-                obj.push_back(value_type(arg1, arg2, arg3, arg4, arg5));
+                obj.push_back(value_type(forward<ARG1>(arg1), forward<ARG2>(arg2), forward<ARG3>(arg3), forward<ARG4>(arg4), forward<ARG5>(arg5)));
             }
         #endif
     } // namespace unboost
