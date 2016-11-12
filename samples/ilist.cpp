58c2786490e80f141b9b05b1bc5e0d1dbe3676d0
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2016-11-12 17:07:28 +0900

    add ilist

---
+++ samples/ilist.cpp
@@ -0,0 +1,58 @@
+// ilist.cpp --- Unboost sample
+//////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <list>
+
+#ifdef CXX11
+    #include <initializer_list>
+#else   // Unboost
+    #include <unboost/ilist.hpp>
+#endif
+
+#ifdef CXX11
+    std::vector<std::string> strs = {
+        "test1",
+        "test2",
+        "test3"
+    };
+#else   // Unboost
+    UNBOOST_ILIST_DEFINE(const char*, strs_contents) = {
+        "test1",
+        "test2",
+        "test3"
+    };
+    std::vector<std::string> strs;
+    UNBOOST_ILIST_INIT(strs, strs_contents);
+#endif  // Unboost
+
+int main(void) {
+    std::cout << "ilist" << std::endl;
+
+    assert(strs[0] == "test1");
+    assert(strs[1] == "test2");
+    assert(strs[2] == "test3");
+    assert(strs.size() == 3);
+
+#ifdef CXX11
+    strs = {
+        "new_value1", "new_value2"
+    };
+#else   // Unboost
+    UNBOOST_ILIST_DEFINE(const char *, strs_new_contents) = {
+        "new_value1", "new_value2"
+    };
+    UNBOOST_ILIST_ASSIGN(strs, strs_new_contents);
+#endif  // Unboost
+
+    assert(strs[0] == "new_value1");
+    assert(strs[1] == "new_value2");
+    assert(strs.size() == 2);
+
+    std::cout << "success" << std::endl;
+
+    return 0;
+} // main
---
+++ unboost/ilist.hpp
@@ -0,0 +1,144 @@
+// ilist.hpp --- Unboost initializer list
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef UNBOOST_TYPE_ILIST_HPP_
+#define UNBOOST_TYPE_ILIST_HPP_
+
+#include "unboost.hpp"
+#include "rv_ref.hpp"   // for rvalue reference
+
+// If not choosed, choose one
+#if ((defined(UNBOOST_USE_CXX11_ILIST) + defined(UNBOOST_USE_UNBOOST_ILIST)) == 0)
+    #ifdef UNBOOST_USE_CXX11
+        #define UNBOOST_USE_CXX11_ILIST
+    #else
+        #define UNBOOST_USE_UNBOOST_ILIST
+    #endif
+#endif
+
+// Adapt choosed one
+#ifdef UNBOOST_USE_CXX11_ILIST
+    #include <initializer_list>     // for std::initializer
+    namespace unboost {
+        using std::initializer_list;
+        struct initializer {
+            template <class T, typename VALUE>
+            initializer(T& obj, const std::initializer_list<VALUE>& ilist) {
+                obj = ilist;
+            }
+        };
+    } // namespace unboost
+    #define UNBOOST_ILIST_DEFINE(type,name)         unboost::initializer_list<type> name
+    #define UNBOOST_ILIST_INIT(obj,ilist_name)      unboost::initializer initializer_##__LINE__((obj), (ilist_name))
+    #define UNBOOST_ILIST_ASSIGN(obj,ilist_name)    ((obj) = (ilist_name))
+#elif defined(UNBOOST_USE_UNBOOST_ILIST)
+    #include <vector>
+    #include <list>
+    namespace unboost {
+        template <typename T>
+        struct initializer_list {
+            typedef T           value_type;
+            typedef T&          reference;
+            typedef const T&    const_reference;
+            typedef size_t      size_type;
+            typedef const T*    iterator;
+            typedef const T*    const_iterator;
+
+            initializer_list() {
+                m_ptr = NULL;
+                m_count = 0;
+            }
+
+            template <typename VALUE, size_t N>
+            initializer_list(const VALUE (&array)[N]) {
+                m_ptr = array;
+                m_count = N;
+            }
+
+            template <typename VALUE>
+            initializer_list(const VALUE *ptr, size_type N) {
+                m_ptr = ptr;
+                m_count = N;
+            }
+
+            size_type size() const { return m_count; }
+            const T *begin() const {
+                return &m_ptr[0];
+            }
+            const T *end() const {
+                return &m_ptr[m_count];
+            }
+
+            T& operator[](size_type i) {
+                return m_ptr[i];
+            }
+            const T& operator[](size_type i) const {
+                return m_ptr[i];
+            }
+
+        public:
+            const T *       m_ptr;
+            size_t          m_count;
+        };
+
+        template <typename E>
+        const E* begin(const initializer_list<E>& il) {
+            return il.begin();
+        }
+        template <typename E>
+        const E* end(const initializer_list<E>& il) {
+            return il.end();
+        }
+
+        template <typename T, typename VALUE>
+        inline std::vector<T>&
+        _ilist_assign(std::vector<T>& obj, const initializer_list<VALUE>& ilist) {
+            obj.clear();
+            const size_t count = ilist.size();
+            obj.reserve(count);
+            for (size_t i = 0; i < count; ++i) {
+                obj.push_back(ilist[i]);
+            }
+            return obj;
+        }
+
+        template <typename T, typename VALUE, size_t N>
+        inline std::vector<T>&
+        _ilist_assign(std::vector<T>& obj, const VALUE (&array)[N]) {
+            initializer_list<VALUE> ilist(array);
+            return _ilist_assign(obj, ilist);
+        }
+
+        template <typename T, typename VALUE>
+        inline std::list<T>&
+        _ilist_assign(std::list<T>& obj, const initializer_list<VALUE>& ilist) {
+            obj.clear();
+            const size_t count = ilist.size();
+            for (size_t i = 0; i < count; ++i) {
+                obj.insert(ilist[i]);
+            }
+            return obj;
+        }
+
+        template <typename T, typename VALUE, size_t N>
+        inline std::list<T>&
+        _ilist_assign(std::list<T>& obj, const VALUE (&array)[N]) {
+            initializer_list<VALUE> ilist(array);
+            return _ilist_assign(obj, ilist);
+        }
+
+        struct initializer {
+            template <typename OBJ_T, typename VALUE, size_t N>
+            initializer(OBJ_T& obj, const VALUE (&array)[N]) {
+                _ilist_assign(obj, initializer_list<VALUE>(array, N));
+            }
+        };
+    } // namespace unboost
+    #define UNBOOST_ILIST_DEFINE(type,name)     type name[]
+    #define UNBOOST_ILIST_INIT(obj,name)        unboost::initializer initializer_##__LINE__((obj), (name))
+    #define UNBOOST_ILIST_ASSIGN(obj,name)      unboost::_ilist_assign((obj), (name))
+#else
+    #error Your compiler is not supported yet. You lose.
+#endif
+
+#endif  // ndef UNBOOST_TYPE_ILIST_HPP_
