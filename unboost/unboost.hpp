a92e89dddbedcc25cfb3094bb5b20a6cd5b81a0d
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2016-11-20 15:41:50 +0900

    version up to 36

--- unboost/unboost.hpp
+++ unboost/unboost.hpp
@@ -4,8 +4,8 @@
 #ifndef UNBOOST_CORE_HPP_
 #define UNBOOST_CORE_HPP_
 
-#define UNBOOST_VERSION             35
-#define UNBOOST_VERSION_STRING      "Unboost 35 by Katayama Hirofumi MZ"
+#define UNBOOST_VERSION             36
+#define UNBOOST_VERSION_STRING      "Unboost 36 by Katayama Hirofumi MZ"
 
 #ifndef _CRT_SECURE_NO_WARNINGS
     #define _CRT_SECURE_NO_WARNINGS
